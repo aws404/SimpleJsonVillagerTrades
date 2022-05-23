54f5fb7459c5aa0d8ce6620ef18a75b52a14bc9c
aws404 <aws40404@gmail.com>
2022-05-23 19:15:46 +1000

    Add client initialiser

--- src/main/java/com/github/aws404/sjvt/SimpleJsonVillagerTradesMod.java
+++ src/main/java/com/github/aws404/sjvt/SimpleJsonVillagerTradesMod.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import com.github.aws404.sjvt.trade_offers.TradeOfferFactoryType;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -15,14 +16,14 @@ import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-public class SimpleJsonVillagerTradesMod implements DedicatedServerModInitializer {
+public class SimpleJsonVillagerTradesMod implements ModInitializer {
 	public static final String MOD_ID = "sjvt";
 	public static final Logger LOGGER = LoggerFactory.getLogger("SimpleJsonVillagerTrades");
 	public static final Registry<TradeOfferFactoryType<?>> TRADE_OFFER_FACTORY_REGISTRY = FabricRegistryBuilder.<TradeOfferFactoryType<?>>createSimple(null, SimpleJsonVillagerTradesMod.id("trade_offer_factories")).buildAndRegister();
 	public static final TradeOfferManager TRADE_OFFER_MANAGER = new TradeOfferManager();
 
 	@Override
-	public void onInitializeServer() {
+	public void onInitialize() {
 		LOGGER.info("Starting SimpleJsonVillagerTrades!");
 		ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(TRADE_OFFER_MANAGER);
 
